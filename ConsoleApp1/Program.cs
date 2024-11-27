30e087a75ed2991b9577b4700a47cee94ace74d1
nihaalxz <nihaal1010@gmail.com>
2024-11-27 14:07:11 +0530

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine(3+4);
+int x=12,y=22;
+Console.WriteLine(x+y);
+int a = 1, b = -1;
+Console.WriteLine(a-b);
+Console.ReadLine();
